a0a21f90e6ad19c3a58824cebe39e82fce9ced6c
Tara Grimm <tararochegrimm@gmail.com>
2017-12-14 11:51:23 -0500

    Added rack-cors gem. Added middleware for rack:cors.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    #middleware that restricts the origins to our front-end app at localhost:3000 and allows access to the standard REST API endpoint methods for all resources.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
