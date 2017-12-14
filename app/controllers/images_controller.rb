befdd1da4aedb8f1b4a2e9b098108dce2bb14758
Tara Grimm <tararochegrimm@gmail.com>
2017-12-14 13:42:29 -0500

    Added create and update actions.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,23 @@
 class ImagesController < ApplicationController
   def index
-    @images = Image.all
+    @images = Image.order("created_at DESC")
     render json: @images
   end
+
+  def create
+    @image = Image.create(image_params)
+    render json: @image
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update_attributes(image_params)
+    render json: @image
+  end
+
+  private
+  
+    def image_params
+      params.require(:image).permit(:url, :alt, :caption)
+    end
 end
